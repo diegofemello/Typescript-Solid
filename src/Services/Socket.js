81d41f808cb6dc14999254485d4f82b9ae581be7
Diego Mello <diegofemello@gmail.com>
2021-07-16 19:16:41 -0300

    Limpando repositório

--- src/Services/Socket.js
+++
@@ -1,8 +0,0 @@
-import socketIOClient from 'socket.io-client';
-
-const url = 'http://3.142.86.155';
-// const url = 'http://192.168.0.110:3000';
-
-const socket = socketIOClient(url);
-
-export default socket;
